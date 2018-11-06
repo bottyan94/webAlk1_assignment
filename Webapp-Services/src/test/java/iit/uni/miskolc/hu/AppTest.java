3fd80f34a793dc37991fcf55f520f4991dbed8e5
bottyan <bottyan94@gmail.com>
2018-11-06 18:26:15 +0100

    First step: directory, gitignore, dependency

---
+++ Webapp-Services/src/test/java/iit/uni/miskolc/hu/AppTest.java
@@ -0,0 +1,20 @@
+package iit.uni.miskolc.hu;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
