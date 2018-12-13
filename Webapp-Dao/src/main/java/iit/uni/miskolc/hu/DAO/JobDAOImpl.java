f031452f81f958272f1d3e96bf6da0953975ac25
bottyan <bottyan94@gmail.com>
2018-12-13 07:11:18 +0100

    last modified

--- Webapp-Dao/src/main/java/iit/uni/miskolc/hu/DAO/JobDAOImpl.java
+++ Webapp-Dao/src/main/java/iit/uni/miskolc/hu/DAO/JobDAOImpl.java
@@ -19,9 +19,9 @@ public class JobDAOImpl implements JobDAO {
         jobs.add(new Job(1, "Dog walking", CASUAL, "Playing with dog.", 500, PRIMARY, "Tomi"));
         jobs.add(new Job(2, "Cashier", CASUAL, "Work with money.", 800, HIGH_SCHOOL, "Tomi"));
         jobs.add(new Job(3, "Dummy1", TRAINEE, "Making coffee.", 1000, PRIMARY, "Bottyan Tamas"));
-        jobs.add(new Job(4, "Dummy2", DEVELOPER, "Dummy data.", 1100, PRIMARY, "Bottyan Tamas"));
+        jobs.add(new Job(4, "Dummy2", DEVELOPER, "Dummy data.", 1100, PRIMARY, "Test HR"));
         jobs.add(new Job(5, "Dummy3", DUMMY, "Dummy data.", 1200, ACADEMY, "Bottyan Tamas"));
-        jobs.add(new Job(6, "Dummy4", TRAINEE, "Dummy data.", 300, ACADEMY, "asd"));
+        jobs.add(new Job(6, "Dummy4", TRAINEE, "Dummy data.", 300, ACADEMY, "Test HR"));
         jobs.add(new Job(7, "Dummy5", TEST, "Dummy data.", 200, UNIVERSITY, "asd"));
         jobs.add(new Job(8, "Dummy6", TEST, "Dummy data.", 400, UNIVERSITY, "asd"));
 
